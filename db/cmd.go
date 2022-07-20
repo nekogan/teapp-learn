43d91050bcffb516dcdb823a13f91cf30723144f
Дмитрий Коваль <koval_minimum@icloud.com>
2022-07-20 21:21:46 +0300

    define func get user posts

--- db/cmd.go
+++ db/cmd.go
@@ -6,27 +6,49 @@ import (
 	"log"
 	"os"
 	"teapp/models"
-
-	"github.com/jackc/pgx/v4"
 )
 
-func SaveToDB(u models.User, p models.Post) {
-	conn, err := pgx.Connect(context.Background(), DATABASE_URL)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
-	}
-	log.Println("CONNECTED TO POSTGRE")
+func SaveToDB(u *models.User, p models.Post) {
+	conn := Connection()
 	defer conn.Close(context.Background())
-
-	sqlStatement := fmt.Sprintf(`INSERT INTO post (user_id, post_title, post_classification, post_text, post_rating) VALUES ('%d', '%s', '%s', '%s', '%d') RETURNING post_id;`, u.ID, p.Title, p.Classification, p.Text, p.Rating)
+	sqlStatement := fmt.Sprintf(`INSERT INTO post (user_id, post_title, post_classification, post_text, post_rating) 
+		VALUES ('%d', '%s', '%s', '%s', '%d') RETURNING post_id;`,
+		u.ID, p.Title, p.Classification, p.Text, p.Rating)
 	fmt.Println(sqlStatement)
 	post_id := 0
-	err = conn.QueryRow(context.Background(), sqlStatement).Scan(&post_id)
+	err := conn.QueryRow(context.Background(), sqlStatement).Scan(&post_id)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
 		os.Exit(1)
 	}
 	defer conn.Close(context.Background())
 	fmt.Println("New record ID is:", post_id)
+}
+
+func GetUserPosts(usrID int) []models.Post {
+	conn := Connection()
+	defer conn.Close(context.Background())
+
+	sqlStatement := fmt.Sprintf(`select post_id, post_title, post_classification, post_text, post_rating from post where user_id='%d'`, usrID)
+	rows, err := conn.Query(context.Background(), sqlStatement)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+	defer rows.Close()
+
+	var rowSlice []models.Post
+	for rows.Next() {
+		var p models.Post
+		err := rows.Scan(&p.ID, &p.Title, &p.Classification, &p.Text, &p.Rating)
+		if err != nil {
+			log.Fatal(err)
+		}
+		rowSlice = append(rowSlice, p)
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return rowSlice
 }
