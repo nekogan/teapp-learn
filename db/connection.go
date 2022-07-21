1f81d05ba8839194c0db1557397843107e9ac7da
nekogan <koval_minimum@icloud.com>
2022-07-21 09:48:13 +0300

    read config

--- db/connection.go
+++ db/connection.go
@@ -3,24 +3,39 @@ package db
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 
 	"github.com/jackc/pgx/v4"
+	"gopkg.in/yaml.v3"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "nekogan"
-	password = "root"
-	dbname   = "teapp_db"
-)
+type db struct {
+	Host     string `yaml:"host"`
+	Port     uint   `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	Dbname   string `yaml:"dbname"`
+}
 
-var DATABASE_URL = fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+func config() string {
+	d := db{}
+	file, err := ioutil.ReadFile("db/config.yml")
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = yaml.Unmarshal(file, &d)
+	if err != nil {
+		log.Println(err)
+	}
+	return fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable",
+		d.Host, d.Port, d.User, d.Password, d.Dbname)
+}
 
 func Connection() *pgx.Conn {
-	conn, err := pgx.Connect(context.Background(), DATABASE_URL)
+	conn, err := pgx.Connect(context.Background(), config())
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
