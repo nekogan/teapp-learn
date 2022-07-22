22581da83af9e12536816d0587d97d56ab58cb04
nekogan <koval_minimum@icloud.com>
2022-07-22 16:27:32 +0300

    for example :)

--- main.go
+++ main.go
@@ -6,28 +6,52 @@ import (
 	"log"
 	"net/http"
 	db "teapp/db"
-	m "teapp/models"
+	"teapp/models"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
 	router := httprouter.New()
-	router.POST("/", Index)
-	router.GET("/:user", UserPage)
+	router.GET("/", Index)
+	router.GET("/login", Auth)
+	// router.GET("/:user", UserPage)
 	log.Println("STARTING SERVER ON PORT :8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func Index(w http.ResponseWriter, _ *http.Request, ps httprouter.Params) {
-	user := m.NewUser("Nekogan", "password", "ImageURL", "Dima", "Koval")
-	post := m.NewPost("Дракон", "Красный", "Самый лучший чай", 10)
-	db.SaveToDB(user, post, db.Connection(ps.ByName("user")))
+func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	usrName := r.Header.Get("Username")
+	usrPass, err := models.HashPassword(r.Header.Get("Password"))
+	if err != nil {
+		log.Println(err)
+	}
+	newUser := models.NewUser(usrName, usrPass, "Avatar", "Dima", "Koval")
+	db.CreateNewUser(newUser, db.Connection())
+}
+
+func Auth(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	usrName := r.Header.Get("Username")
+	usrPass := r.Header.Get("Password")
+
+	match := db.UserAuth(usrName, usrPass, db.Connection())
+	if !match {
+		fmt.Fprintf(w, "Введены не верные данные для входа")
+		log.Println(match)
+		return
+	}
+	fmt.Fprintf(w, "%s, Добро пожаловать!", usrName)
 }
 
 func UserPage(w http.ResponseWriter, _ *http.Request, ps httprouter.Params) {
-	user := db.GetUser(uint(db.UserID), db.Connection(ps.ByName("user")))
-	posts := db.GetUserPosts(uint(db.UserID), db.Connection(ps.ByName("user")))
+	userId, err := db.GetUserID(ps.ByName("user"), db.Connection())
+	if err != nil {
+		fmt.Fprintf(w, "%v", err)
+		return
+	}
+
+	user := db.GetUser(userId, db.Connection())
+	posts := db.GetUserPosts(userId, db.Connection())
 	userInfo, err := json.MarshalIndent(user, "", "   ")
 	if err != nil {
 		log.Println(err)
