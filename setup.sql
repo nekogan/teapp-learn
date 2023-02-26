4c4ed21c1ba706da1f6ac7e3d7e9419f9b547180
Дмитрий Коваль <koval_minimum@icloud.com>
2023-02-26 14:34:41 +0300

    correct

--- setup.sql
+++ setup.sql
@@ -1,7 +1,7 @@
 create table users (
 	user_id int not null GENERATED ALWAYS AS identity,
 	primary key(user_id),
-	user_name varchar(255) not null,
+	user_name varchar(30) not null,
 	user_pass varchar(255) not null,
 	user_avatar varchar,
 	user_firstname varchar(100),
@@ -12,8 +12,8 @@ create table post (
 	post_id int not null GENERATED ALWAYS AS identity,
 	user_id int,
 	foreign key(user_id) REFERENCES users(user_id) on delete cascade on update cascade,
-	post_title varchar(40),
-	post_category varchar(1000),
-	post_text text,
-	post_rating int
+	post_title varchar(255),
+	post_category varchar(60),
+	post_tags varchar(255),
+	post_text text
 );
